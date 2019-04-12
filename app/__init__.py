660c021d8f01a813153b776d0950bc4be0bb3baf
Amir Edin <amiredin@yahoo.com>
2019-04-12 15:57:25 +0300

    add request folder

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,13 @@
 from flask import Flask 
 
+
 # inttializing application 
-app = Flask(__name__)
+
+app = Flask(__name__,instance_relative_config = True)
+
+# Setting up configuration
+
+app.config.from_pyfile('config.py')
+
 
 from app import views
